ac689c6454c9cb4ec8cbe3cb7cf7c6c7a7e8f33c
KYUNGJAE LEE <kyungjaelee@KYUNGJAEui-MacBookAir.local>
2022-01-24 19:44:05 +0900

    modify route

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route
+          basename={process.env.PUBLIC_URL}
+          path="/movie/:id"
+          element={<Detail />}
+        />
+        <Route basename={process.env.PUBLIC_URL} path="/" element={<Home />} />
       </Routes>
     </Router>
   );
